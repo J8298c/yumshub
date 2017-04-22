f064d1365fe090a65f7f1bf4a7561522eb58a440
j8298c <juliocmojica@gmail.com>
2017-04-22 07:10:03 -0400

    fixed intro page css

--- src/component/Header/Intro.js
+++ src/component/Header/Intro.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import {Button, Glyphicon} from 'react-bootstrap';
 import {Link} from 'react-router';
+import HeadContainer from './HeadContainer';
 
 export default function Intro() {
         return (
             <div>
-                <h1>Welcome to YumsHub</h1>
-                <p>The app that lets you order food without having to pick up your phone</p>
-                <div>We send you all the restuarants nearby that deliver you pick the restaurant you want. Now that you choose your restaurant you will be taken to the menu page.</div>
-                <div>
+                <HeadContainer />
+                <h1 className="help-title">Welcome to YumsHub</h1>
+                <div className="help-para">We send you all the restuarants nearby that deliver you pick the restaurant you want. Now that you choose your restaurant you will be taken to the menu page.
                 Ahh! The menu page one step closer to your doorbell ringing, with your delicious food delivery, so lets wrap this up. While browsing through the menu find what you want to eat and click the <span><Button className='demo-button'>add to cart</Button></span> and just like that its added to your cart.
-                </div>
-                <div>
                 Nice now on to your final step click on the <span><Glyphicon className="demo-cart" glyph="shopping-cart"/></span> from here double check your order click pay and your foods on its way.
                 </div>
-                <div><Link to="/">Proceed to the app</Link></div>
+                <div className="help-link"><Link className="help-link" to="/">Proceed to the app</Link></div>
             </div>
         )
     }
